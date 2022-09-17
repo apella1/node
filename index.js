ea9ec631d48d16c0afb07efd3e60257d9df77624
apella1 <johncollons5@gmail.com>
2022-09-17 14:30:29 +0300

    first encounter with node

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('Hello from this end of town');
+
+// using node we can run any javascript file within the terminal and the javascript file extension doesn't have to be included 
+// the node modules shouldn't be deployed with the other files. when in production, the available dependencies in the package.json file can be be reinstalled using 'npm install' 
+
+const person = require('./person');
+
+console.log(person);
+
+const Person = require('./person');
+
+person1 = new Person ('Malcolm', 88);
+
+person1.summary();
+// node is yet to implement the es6 import feature as in other javascript libraries such as react where files are imported from external file paths.
+
+
+// ! Quokka free edition doesn't support file exports
---
+++ person.js
@@ -0,0 +1,24 @@
+const person = { 
+    name: 'John Doe', 
+    age: 67
+}
+
+module.exports = person;
+
+class Person { 
+    constructor (name, age) {
+         this.name = name;
+         this.age = age;
+    } 
+
+    summary () { 
+        console.log(`My name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+module.exports = Person;
+
+// the const modifier can only be used in typescript files when working with class components.
+// this is when trying to have a function expression within the class constructor 
+
+// ? what are module wrapper functions 
