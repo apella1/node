f029f39315da5ea9f065c42d2b184ab2ddd55d45
apella1 <johncollons5@gmail.com>
2022-09-27 22:35:46 +0300

    url and demo

---
+++ reference/url_demo.js
@@ -0,0 +1,33 @@
+const url = require('url');
+
+const myUrl = new URL('http://mywebsite.com/hello.html');
+
+// serialized url
+console.log((myUrl.href));
+console.log(myUrl.toString());
+
+//host (root domain)
+
+console.log(myUrl.host);
+
+// host name - does not get port 
+console.log(myUrl.hostname);
+
+// path name
+console.log(myUrl.pathname);
+
+// serialized query
+console.log(myUrl.search);
+
+// params object 
+console.log(myUrl.searchParams);
+
+// add params 
+myUrl.searchParams.append('abc', '123')
+
+console.log(myUrl.searchParams);
+
+// looping through params 
+myUrl.searchParams.forEach((value, name) => { 
+    console.log(`${name}:${value}`);
+})
